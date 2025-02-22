80ff259db8b4e8b6103c4b9a39595c56d22b60d5
Nguyen Huu Duy <nguyenhuuduy23082004@gmail.com>
2025-02-22 11:35:30 +0700

    Test dataloader

---
+++ test_MaskZhangTrain_dataloader.py
@@ -0,0 +1,22 @@
+import torch
+from torchvision import transforms
+from pseudo_zhang import MaskZhangTrain
+from PIL import Image
+
+if __name__ == "__main__":  
+    test_transform = transforms.Compose([
+            transforms.Resize(size=224, interpolation=Image.BICUBIC),
+            transforms.CenterCrop(size=(224, 224)),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.39799, 0.39799, 0.39799], std=[0.32721349, 0.32721349, 0.32721349])
+        ])
+    test_dataset = MaskZhangTrain(r"ChestXray_Classification\3_images_to_test", train=False, transforms=test_transform)
+    testloader = torch.utils.data.DataLoader(test_dataset, batch_size=2048, shuffle=False, num_workers=32)
+
+    print(len(testloader))
+    for i, (img, labels, masks, position_names) in enumerate(testloader):
+        print(i)
+        print(img.shape)
+        print(labels)
+        print(masks)
+        print(position_names)
